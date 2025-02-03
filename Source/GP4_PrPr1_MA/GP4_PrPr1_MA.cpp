c8de5daae8d30379c4dbe605ee9cd556f0f6636b
MelanieLAlvarado <lanicollege22@gmail.com>
2025-02-03 16:50:55 -0600

    added project

---
+++ Source/GP4_PrPr1_MA/GP4_PrPr1_MA.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "GP4_PrPr1_MA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GP4_PrPr1_MA, "GP4_PrPr1_MA" );
